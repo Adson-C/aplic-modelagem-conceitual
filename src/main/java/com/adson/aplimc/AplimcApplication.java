749a0d80d3a996d94c2056da63c684bec072a738
Adson Santos <adsonconcecao@yahoo.com.br>
2021-08-08 17:55:01 -0300

    Projeto criado

---
+++ src/main/java/com/adson/aplimc/AplimcApplication.java
@@ -0,0 +1,13 @@
+package com.adson.aplimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplimcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adson/aplimc/AplimcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adson.aplimc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplimcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
