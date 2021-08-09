7694e465a91e715eafc43d341a0f2a24283122d5
Adson Santos <adsonconcecao@yahoo.com.br>
2021-08-09 20:57:13 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/adson/aplimc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.adson.aplimc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.adson.aplimc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardErrror> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardErrror err = new StandardErrror(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
--- src/main/java/com/adson/aplimc/services/CategoriaService.java
+++ src/main/java/com/adson/aplimc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.adson.aplimc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adson.aplimc.domain.Categoria;
 import com.adson.aplimc.repositories.CategoriaRepository;
+import com.adson.aplimc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
