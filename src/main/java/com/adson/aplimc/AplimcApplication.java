844ba75ec3165b963bea13d48aedbd28fe549189
Adson Santos <adsonconcecao@yahoo.com.br>
2021-08-09 00:55:20 -0300

    Criando operacao de instanciacao

--- src/main/java/com/adson/aplimc/AplimcApplication.java
+++ src/main/java/com/adson/aplimc/AplimcApplication.java
@@ -1,13 +1,33 @@
 package com.adson.aplimc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adson.aplimc.domain.Categoria;
+import com.adson.aplimc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class AplimcApplication {
+public class AplimcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplimcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
