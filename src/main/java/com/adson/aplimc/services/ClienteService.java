46a21fab9417e980358aecbd42ea4573bdeac8c6
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-13 12:11:59 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/adson/aplimc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.adson.aplimc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adson.aplimc.domain.Cliente;
+import com.adson.aplimc.repositories.ClienteRepository;
+import com.adson.aplimc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
