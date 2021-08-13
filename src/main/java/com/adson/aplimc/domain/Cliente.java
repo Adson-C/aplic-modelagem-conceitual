6b4bb387d7046d838fb26d76820aefa3a28179eb
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-13 12:55:14 -0300

    Pedido, EstadoPagamento e Pagamento - part 1

--- src/main/java/com/adson/aplimc/domain/Cliente.java
+++ src/main/java/com/adson/aplimc/domain/Cliente.java
@@ -38,6 +38,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<String>();
 	
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
+	
 	public Cliente() {
 	}
 
@@ -105,6 +107,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -122,4 +132,5 @@ public class Cliente implements Serializable {
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
