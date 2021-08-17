552491029ce910c14c75b3c7851e5671f960f4b9
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-16 23:00:45 -0300

    Validação customizada CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/adson/aplimc/dto/ClienteNewDTO.java
+++ src/main/java/com/adson/aplimc/dto/ClienteNewDTO.java
@@ -2,20 +2,41 @@ package com.adson.aplimc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.adson.aplimc.services.validation.ClientInsert;
+
+@ClientInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preechimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho teve ser de 5 a 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preechimento obrigatório")
+	@Email(message = "Email Inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preechimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	@NotEmpty(message = "Preechimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preechimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preechimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "Preechimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
---
+++ src/main/java/com/adson/aplimc/services/validation/ClientInsertValidator.java
@@ -0,0 +1,40 @@
+package com.adson.aplimc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.adson.aplimc.domain.enums.TipoCliente;
+import com.adson.aplimc.dto.ClienteNewDTO;
+import com.adson.aplimc.resources.exception.FieldMessage;
+import com.adson.aplimc.services.validation.utils.BR;
+
+public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClientInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNJP invalido"));
+		}
+
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
