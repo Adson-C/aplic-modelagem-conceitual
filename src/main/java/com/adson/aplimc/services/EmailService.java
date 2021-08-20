4042148121a4eb3f52b92c42fdb2ff79884fcbeb
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-20 11:23:45 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/adson/aplimc/services/EmailService.java
+++ src/main/java/com/adson/aplimc/services/EmailService.java
@@ -1,14 +1,18 @@
 package com.adson.aplimc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.adson.aplimc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
-
-
