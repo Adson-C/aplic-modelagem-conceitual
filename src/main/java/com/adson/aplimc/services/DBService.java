610d82d39a07c25f9febad32790e319baed30f4c
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-20 12:10:54 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/adson/aplimc/services/DBService.java
+++ src/main/java/com/adson/aplimc/services/DBService.java
@@ -20,6 +20,7 @@ import com.adson.aplimc.domain.PagamentoComCartao;
 import com.adson.aplimc.domain.Pedido;
 import com.adson.aplimc.domain.Produto;
 import com.adson.aplimc.domain.enums.EstadoPagamento;
+import com.adson.aplimc.domain.enums.Perfil;
 import com.adson.aplimc.domain.enums.TipoCliente;
 import com.adson.aplimc.repositories.CategoriaRepository;
 import com.adson.aplimc.repositories.CidadeRepository;
@@ -124,13 +125,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "adsonconceicao442@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "adsonconcecao@yahoo.com.br", "80236115006", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("875265186", "745259871"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avinida Floriano", "2106", null, "Centro", "37777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
