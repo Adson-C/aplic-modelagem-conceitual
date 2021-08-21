a93072cac65e37672133505bf2dcfc2c94547b26
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-21 00:41:34 -0300

    Implementando autorização

--- src/main/java/com/adson/aplimc/config/SegurityConfig.java
+++ src/main/java/com/adson/aplimc/config/SegurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.adson.aplimc.security.JWTAuthenticationFilter;
+import com.adson.aplimc.security.JWTAuthorizationFilter;
 import com.adson.aplimc.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SegurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
