d75ba627612033f919818e977af4eea4471a0474
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-23 01:19:00 -0300

    Enviando imagem via endpoint

--- src/main/java/com/adson/aplimc/services/ClienteService.java
+++ src/main/java/com/adson/aplimc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.adson.aplimc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.adson.aplimc.domain.Cidade;
 import com.adson.aplimc.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		
@@ -107,6 +112,10 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 		
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
