97ce40cac67511b50be0224bdc3e8981f3d5ba79
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-22 23:55:51 -0300

    Salvando primeiro arquivo no Amazon S#

--- src/main/java/com/adson/aplimc/AplimcApplication.java
+++ src/main/java/com/adson/aplimc/AplimcApplication.java
@@ -1,12 +1,17 @@
 package com.adson.aplimc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adson.aplimc.services.S3Service;
+
 @SpringBootApplication
 public class AplimcApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AplimcApplication.class, args);
@@ -15,6 +20,8 @@ public class AplimcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("C:\\temp\\fotos\\ads.jpg");
+		
 	}
 
 }
