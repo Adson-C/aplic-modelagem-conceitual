4fb868a3130283744d9772a163a84bf9cf464b90
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-08-22 21:27:02 -0300

    Esqueci Senha

--- src/main/java/com/adson/aplimc/resources/AuthResource.java
+++ src/main/java/com/adson/aplimc/resources/AuthResource.java
@@ -2,15 +2,19 @@ package com.adson.aplimc.resources;
 
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adson.aplimc.dto.EmailDTO;
 import com.adson.aplimc.security.JWTUtil;
 import com.adson.aplimc.security.UserSS;
+import com.adson.aplimc.services.AuthService;
 import com.adson.aplimc.services.UserService;
 
 @RestController
@@ -19,12 +23,21 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto ) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/adson/aplimc/services/EmailService.java
+++ src/main/java/com/adson/aplimc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.adson.aplimc.domain.Cliente;
 import com.adson.aplimc.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,5 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
