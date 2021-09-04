51b4b44b4608f6d1774cfe8219d184cc76258540
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-09-04 14:08:34 -0300

    Documentação com Swagger part-1

--- src/main/java/com/adson/aplimc/config/S3Config.java
+++ src/main/java/com/adson/aplimc/config/S3Config.java
@@ -1,3 +1,4 @@
+
 package com.adson.aplimc.config;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -12,25 +13,24 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class S3Config {
-	
+
 	@Value("${aws.access_key_id}")
 	private String awsId;
-	
+
 	@Value("${aws.secret_access_key}")
 	private String awsKey;
-	
+
 	@Value("${s3.region}")
 	private String region;
-	
-	
+
 	@Bean
 	public AmazonS3 s3client() {
 		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsId, awsKey);
 		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
 				.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
-		
+
 		return s3client;
-				
+
 	}
 
 }
